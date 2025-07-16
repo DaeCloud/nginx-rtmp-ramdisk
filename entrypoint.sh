a220319098c3ef4fefa7cd194dab6aebacb3e20c
Daelin Campleman <127960675+Daelin-Campleman@users.noreply.github.com>
2025-07-16 16:27:54 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,7 +84,7 @@ http {
             # XML stylesheet to view RTMP stats.
             # Copy stat.xsl wherever you want
             # and put the full directory path here
-            root /mnt/ramdisk/stat.xsl;
+            root /usr/local/nginx/html;
         }
 
         location /health {
